1834df574c4d2a5de02941a4c9adac02c382a240
makaron4ik3000@gmail.com <bob@example.com>
2020-11-22 18:35:10 +0300

    commit

---
+++ app/src/main/java/com/example/recycle_view/BookListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.recycle_view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.element_book.view.*
+
+class BookListAdapter(val context: Context, val categoryItem: List<BookinfoClass>) :
+    RecyclerView.Adapter<BookListAdapter.BookHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookHolder {
+        return BookHolder(
+            LayoutInflater.from(context).inflate(R.layout.element_book, parent, false)
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: BookHolder, position: Int) {
+
+        val currentItem = categoryItem[position]
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+    }
+
+
+    override fun getItemCount(): Int {
+        return categoryItem.size
+    }
+
+
+    class BookHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.imageBook
+        val textView1: TextView = itemView.name
+        val textView2: TextView = itemView.janr
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/recycle_view/BookinfoClass.kt
@@ -0,0 +1,4 @@
+package com.example.recycle_view
+
+class BookinfoClass(val imageResource: Int, val text1: String, val text2: String) {
+}
